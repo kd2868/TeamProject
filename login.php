9c8f4aec9e28067f3591cd93883ebb97325ce3af
urosantic <u18anti@siena.edu>
2016-11-07 22:05:44 -0500

    Added link on submit to professor landing page

--- login.php
+++ login.php
@@ -2,10 +2,14 @@
 	require 'functions/function_index.php';
 	
 	echo createHeader('Login Form');
-	
-	date_default_timezone_set('America/New_York');
-	$displayDate = date(" F j, Y, g:i a");
-	echo createLoginForm($displayDate);
+	if($_GET['action']!='submit'){
+		date_default_timezone_set('America/New_York');
+		$displayDate = date(" F j, Y, g:i a");
+		echo createLoginForm($displayDate);
+	}
+	else{
+		header('location: /~perm_team4_2016/team_project/professor_landing.php');
+	}
 	echo createFooter();
 	
 ?>
---
+++ professor_landing.php
@@ -0,0 +1,12 @@
+<?php
+	require 'functions/function_index.php';
+
+	
+	
+	echo createHeader('professor Landing');
+	
+	echo 'Professor Landing Page';
+	
+	echo createFooter();
+
+?>
