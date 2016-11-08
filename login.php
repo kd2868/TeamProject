7271518688e8f2c58ff1b7ddb76a7eacb1f49007
urosantic <u18anti@siena.edu>
2016-11-07 21:47:28 -0500

    Testing Upload from Server

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
 	require 'functions/function_index.php';
+	
 	echo createHeader('Login Form');
+	
 	date_default_timezone_set('America/New_York');
 	$displayDate = date(" F j, Y, g:i a");
 	echo createLoginForm($displayDate);
